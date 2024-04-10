ae827e0a6b87502345843a92c25f44c1688d0f9f
Jacob Harris <jacobh12jacob876@gmail.com>
2024-04-09 23:17:59 -0500

    changed port

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ app.post('/upload', upload.single('document'), async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
